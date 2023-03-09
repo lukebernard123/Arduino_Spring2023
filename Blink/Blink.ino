3b93df4c6cfb7fae3becd9ca40cd96756a3b96c2
Luke Bernard <02luke.bernard@gulllakecs.org>
2023-03-09 10:57:59 -0500

    Added a third pin 10, rewrote code.

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -31,14 +31,31 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(11, HIGH);    // turn the LED off by making the voltage LOW
   digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  digitalWrite(10, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(10, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(12, HIGH);    // turn the LED off by making the voltage LOW
+  digitalWrite(10, HIGH);    // turn the LED off by making the voltage LOW
+  delay(1000); 
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(10, LOW);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(11, HIGH); 
+  digitalWrite(10, HIGH); // turn the LED off by making the voltage LOW
+  delay(1000); 
+  digitalWrite(11, LOW); 
+  digitalWrite(10, LOW); // turn the LED off by making the voltage LOW
+  delay(1000); 
 }
