25763a6e16c90969f9f261d7633ad713f2076339
Luke Bernard <02luke.bernard@gulllakecs.org>
2023-03-16 14:08:55 -0400

    Initial BlindSpot Monitor Commit

---
+++ BlindSpot_Monitor/BlindSpot_Monitor.ino
@@ -0,0 +1,18 @@
+
+//programmer: Luke 
+//Date: 3.16.23
+//Program: Blind Spot Monitor
+
+/*
+ * This program will determine distances appraoching vehicles that are in your blindspot and turn on warning light
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
